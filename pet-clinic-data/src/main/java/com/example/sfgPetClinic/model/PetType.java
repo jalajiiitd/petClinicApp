2365575547536d497b24a89541acc3bcf2011b90
jalajiiitd <jalaj17017@iiitd.ac.in>
2022-10-17 15:32:00 +0530

    Refactored interfaces to common CrudService

--- pet-clinic-data/src/main/java/com/example/sfgPetClinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/example/sfgPetClinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.example.sfgPetClinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
 	private String name;
 
--- pet-clinic-data/src/main/java/com/example/sfgPetClinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/example/sfgPetClinic/services/OwnerService.java
@@ -1,14 +1,9 @@
 package com.example.sfgPetClinic.services;
 
-import java.util.Set;
-
 import com.example.sfgPetClinic.model.Owner;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 	
-	Owner findById(Long id);
-	Owner save(Owner owner);
 	Owner findByLastName(String lastName);
-	Set<Owner> findAll();
 
 }
