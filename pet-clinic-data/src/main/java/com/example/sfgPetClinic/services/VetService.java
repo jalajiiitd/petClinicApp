e82f8455aac2782c612a30a1688eb98d79722d38
jalajiiitd <jalaj17017@iiitd.ac.in>
2022-10-18 09:17:05 +0530

    Implemented Map Based Services

--- pet-clinic-data/src/main/java/com/example/sfgPetClinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/example/sfgPetClinic/services/VetService.java
@@ -1,13 +1,8 @@
 package com.example.sfgPetClinic.services;
 
-import java.util.Set;
 
 import com.example.sfgPetClinic.model.Vet;
 
-public interface VetService {
-
-	Vet findById(Long id);
-	Vet save(Vet vet);
-	Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 	
 }
