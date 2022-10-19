97f69cb2613ab7688ab2b2582c227646c12da00d
jalajiiitd <jalaj17017@iiitd.ac.in>
2022-10-19 13:02:53 +0530

    Managed auto increment of Id values

--- pet-clinic-data/src/main/java/com/example/sfgPetClinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/example/sfgPetClinic/services/map/AbstractMapService.java
@@ -1,15 +1,19 @@
 package com.example.sfgPetClinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
+import com.example.sfgPetClinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T,ID> {
 
-	protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
+
+	protected Map<Long, T> map = new HashMap<>();
 	Set<T> findAll() {
 		return new HashSet<>(map.values());
 	}
@@ -18,8 +22,17 @@ public abstract class AbstractMapService<T,ID> {
 		return map.get(id);
 	}
 	
-	T save(ID id, T obj) {
-		map.put(id, obj);
+	T save(T obj) {
+		
+		if(obj != null) {
+			if(obj.getId() == null) {
+				obj.setId(getNextId());
+			}
+			map.put(obj.getId(), obj);
+		}
+		else {
+			throw new RuntimeException("Object cannot be null");
+		}
 		return obj;
 	}
 	
@@ -28,13 +41,24 @@ public abstract class AbstractMapService<T,ID> {
 	}
 	
 	void delete(T obj) {
-		Iterator<Map.Entry<ID, T>> iterator = map.entrySet().iterator();
+		Iterator<Map.Entry<Long, T>> iterator = map.entrySet().iterator();
 
 		while (iterator.hasNext()) {
-		    Map.Entry<ID,T> entry = iterator.next();
+		    Map.Entry<Long,T> entry = iterator.next();
 		    if (obj.equals(entry.getValue())) {
 		        iterator.remove();
 		    }
 		}
+	}
+	
+	private Long getNextId() {
+		Long nextId = null;
+		try {
+			nextId = Collections.max(map.keySet()) + 1;
+		}
+		catch(NoSuchElementException e) {
+			nextId = 1L;
+		}
+		return nextId;
 	}
 }
