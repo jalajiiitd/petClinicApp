f089092a93f1d725742a60b71207d6ffbdcac52a
jalajiiitd <jalaj17017@iiitd.ac.in>
2022-10-19 12:01:06 +0530

    Returning owners list and updated ownerController

--- pet-clinic-web/src/main/java/com/example/sfgPetClinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/sfgPetClinic/controllers/OwnerController.java
@@ -1,15 +1,25 @@
 package com.example.sfgPetClinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.sfgPetClinic.services.OwnerService;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 	
-	@RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
 
+	@RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(Model model){
+		
+		model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 
